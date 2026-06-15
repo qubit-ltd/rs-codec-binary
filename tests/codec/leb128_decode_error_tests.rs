05a4e856d6533e4b6f6644a753db840adae9f463
starfish <starfish.hu@gmail.com>
2026-06-15 14:06:48 +0800

    test(codec): add CodecDecodeSignal assertions to leb128 error tests

--- tests/codec/leb128_decode_error_tests.rs
+++ tests/codec/leb128_decode_error_tests.rs
@@ -1,5 +1,6 @@
 use core::num::NonZeroUsize;
 
+use qubit_codec::CodecDecodeSignal;
 use qubit_codec_binary::{Leb128DecodeError, Leb128DecodeErrorKind};
 
 #[test]
@@ -12,7 +13,9 @@ fn test_incomplete_stores_start_error_required_available_and_additional_units() 
     assert_eq!(5, error.start_index());
     assert_eq!(7, error.error_index());
     assert_eq!(None, error.consumed());
+    assert_eq!(None, error.consumed_units());
     assert_eq!(Some(required), error.required());
+    assert_eq!(Some(3), error.required_total());
     assert_eq!(Some(2), error.available());
     assert_eq!(NonZeroUsize::new(1), error.additional());
     assert_eq!(
@@ -56,7 +59,10 @@ fn test_invalid_errors_store_start_error_and_consumed_units() {
     assert_eq!(10, noncanonical.error_index());
     assert_eq!(Some(malformed_consumed), malformed.consumed());
     assert_eq!(Some(noncanonical_consumed), noncanonical.consumed());
+    assert_eq!(Some(malformed_consumed), malformed.consumed_units());
+    assert_eq!(Some(noncanonical_consumed), noncanonical.consumed_units());
     assert_eq!(None, malformed.required());
+    assert_eq!(None, malformed.required_total());
     assert_eq!(None, malformed.additional());
     assert_eq!(None, noncanonical.available());
     assert_eq!(
